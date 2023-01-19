2c07c1665ef1346be96f88551e2cfa425fbc1a8e
aarzoogupta <118600162+aarzoogupta@users.noreply.github.com>
2023-01-19 11:26:35 +0530

    Submitting Q6 of Assignment 1

---
+++ a1_q6.cpp
@@ -0,0 +1,14 @@
+//reverse a number
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,rev=0;
+    cin>>num;
+    while (num!=0)
+    {
+        rev=(rev*10)+(num%10);
+        num/=10;
+    }
+    cout<<"The reversed number is:"<<rev;
+}
